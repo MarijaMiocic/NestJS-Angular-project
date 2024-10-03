1c5b65ec7fb23651266d920e1b2495d5728ce46f
Marija <mmiocic122@student.unizd.hr>
2024-10-03 20:18:56 +0200

    Add backend authentication and user modules

--- recipe-sharing-backend/src/app.module.ts
+++ recipe-sharing-backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity'; // Import User entiteta
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // Koristi SQLite kao bazu podataka
+      database: 'recipe-sharing.db', // Naziv SQLite datoteke
+      entities: [User], // Registracija entiteta (User)
+      synchronize: true, // Automatski kreira/ ažurira tablice, koristi se samo u razvoju
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
